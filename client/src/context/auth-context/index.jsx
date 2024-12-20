8a53775c7fe26918c53e8c087dae9cb640c83da1
prakhar362 <prakharshri2005@gmail.com>
2024-12-20 13:11:09 +0530

    login/signup done ,start of instructor page

--- client/src/context/auth-context/index.jsx
+++ client/src/context/auth-context/index.jsx
@@ -2,27 +2,80 @@ import { createContext } from "react";
 import { useState } from "react";
 import { initialSignInFormData, initialSignUpFormData } from "@/config";
 
-import { registerService } from "@/services";
+import { registerService,loginService } from "@/services";
 export const AuthContext=createContext(null);
 
 export default function AuthProvider({children})
 {
     const [signInFormData, setSignInFormData] = useState(initialSignInFormData);
     const [signUpFormData, setSignUpFormData] = useState(initialSignUpFormData);
-
+    const [auth, setAuth] = useState({
+        authenticate: false,
+        user: null,
+      });
     async function handleRegisterUser(){
         event.preventDefault();
         const data=await registerService(signUpFormData);
         console.log(data);
+        
     }
 
+    async function handleLoginUser(){
+        event.preventDefault();
+        const data=await loginService(signInFormData);
+        console.log(data);
+        if(data.success){
+            sessionStorage.setItem('accessToken',JSON.stringify(data.data.accessToken));
+            setAuth({
+                authenticate:true,
+                user: data.user
+            })
+        }
+        else{
+            setAuth({
+                authenticate:false,
+                user: null,
+            })
 
+        }
+        
+    }
+
+    //check auth-user
+    async function checkAuthUser() {
+        try {
+          const data = await checkAuthService();
+          if (data.success) {
+            setAuth({
+              authenticate: true,
+              user: data.data.user,
+            });
+            setLoading(false);
+          } else {
+            setAuth({
+              authenticate: false,
+              user: null,
+            });
+            setLoading(false);
+          }
+        } catch (error) {
+          console.log(error);
+          if (!error?.response?.data?.success) {
+            setAuth({
+              authenticate: false,
+              user: null,
+            });
+            setLoading(false);
+          }
+        }
+      }
 
     return <AuthContext.Provider value={{
         signInFormData,
         setSignInFormData,
         setSignUpFormData,
         signUpFormData,
-        handleRegisterUser
+        handleRegisterUser,
+        handleLoginUser,
     }}>{children}</AuthContext.Provider>
 }
--- server/controllers/auth-controller/index.js
+++ server/controllers/auth-controller/index.js
@@ -1,7 +1,7 @@
 
 const User=require('../../models/User');
 const bcrypt=require('bcryptjs');
-
+const jwt = require("jsonwebtoken");
 
 const registerUser=async(req,res)=>{
     const{userName,userEmail, password,role }=req.body;
@@ -29,4 +29,42 @@ const registerUser=async(req,res)=>{
     })
 };
 
-module.exports = {registerUser};
+const loginUser = async (req, res) => {
+    const { userEmail, password } = req.body;
+  
+    const checkUser = await User.findOne({ userEmail });
+  
+    if (!checkUser || !(await bcrypt.compare(password, checkUser.password))) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+  
+    const accessToken = jwt.sign(
+      {
+        _id: checkUser._id,
+        userName: checkUser.userName,
+        userEmail: checkUser.userEmail,
+        role: checkUser.role,
+      },
+      "JWT_SECRET",
+      { expiresIn: "120m" }
+    );
+  
+    res.status(200).json({
+      success: true,
+      message: "Logged in successfully",
+      data: {
+        accessToken,
+        user: {
+          _id: checkUser._id,
+          userName: checkUser.userName,
+          userEmail: checkUser.userEmail,
+          role: checkUser.role,
+        },
+      },
+    });
+  };
+
+module.exports = {registerUser,loginUser};
