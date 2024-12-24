1a29c4a14dc38819e72475f129a26c56e6942507
prakhar362 <prakharshri2005@gmail.com>
2024-12-24 10:32:02 +0530

    frontend to backend

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://lms-7rju.onrender.com/",
 });
 
 axiosInstance.interceptors.request.use(
